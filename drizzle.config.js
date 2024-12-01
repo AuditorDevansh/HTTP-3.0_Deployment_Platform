c8dc3b2568e7e27ebb156ed5e2185af3143434bf
Devansh Mishra <166271475+AuditorDevansh@users.noreply.github.com>
2024-12-01 11:23:19 +0530

    first commit

---
+++ drizzle.config.js
@@ -0,0 +1,12 @@
+export default {
+    dialect: "postgresql",
+    schema: "./utils/db/schema.ts",
+    out: "./drizzle",
+  
+    dbCredentials: {
+      url: "postgresql://neondb_owner:FT1LytZMEe3p@ep-falling-heart-a5vuhhbk.us-east-2.aws.neon.tech/neondb?sslmode=require",
+      connectionString:
+        "postgresql://neondb_owner:xyDszWq49UJh@ep-soft-violet-a5vv99sd.us-east-2.aws.neon.tech/http3?sslmode=require",
+    },
+  };
+  
